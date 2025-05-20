d8ba6de38fc88535d36b9e07e2e7cf89749dba4b
DrCalvinho <lughgod@gmail.com>
2025-05-20 14:55:05 +0100

    Update client.lua.lua
    Fix performance and code more clear and see the Debug on console can active/desactive

--- client.lua.lua
+++ client.lua.lua
@@ -1,37 +1,34 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Utility to check if the player has an allowed job
-local function IsJobAllowed()
-    local playerData = QBCore.Functions.GetPlayerData()
-    if not playerData or not playerData.job then return false end
-
-    return Config.AllowedJobs[playerData.job.name] or false
+local function IsJobAllowed(jobName)
+    return Config.AllowedJobs[jobName] or false
 end
 
--- Disable being dragged out of vehicle
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Wait(1000)
+        Wait(1500) -- Better performance
         local ped = PlayerPedId()
-
-        if IsPedInAnyVehicle(ped, false) then
-            local vehicle = GetVehiclePedIsIn(ped, false)
-
-            if Config.LockDoors and GetPedInVehicleSeat(vehicle, -1) == ped then
-                SetVehicleDoorsLocked(vehicle, 2)
-                if Config.Debug then print("[no_dragout] Vehicle doors locked") end
-            end
-
-            local seats = GetVehicleModelNumberOfSeats(GetEntityModel(vehicle))
-            for i = -1, seats - 2 do
-                local seatPed = GetPedInVehicleSeat(vehicle, i)
+        if not IsPedInAnyVehicle(ped, false) then goto continue end
+        local vehicle = GetVehiclePedIsIn(ped, false)
+        if not vehicle or vehicle == 0 then goto continue end
+        if Config.LockDoors and GetPedInVehicleSeat(vehicle, -1) == ped then
+            SetVehicleDoorsLocked(vehicle, 2)
+            if Config.Debug then print("[no_dragout] Vehículo bloqueado") end
+        end
+        local model = GetEntityModel(vehicle)
+        local seatCount = GetVehicleModelNumberOfSeats(model)
+        local playerData = QBCore.Functions.GetPlayerData()
+        local jobName = playerData and playerData.job and playerData.job.name
+        local allowed = jobName and IsJobAllowed(jobName)
+        if not allowed then
+            for seat = -1, seatCount - 2 do
+                local seatPed = GetPedInVehicleSeat(vehicle, seat)
                 if seatPed and seatPed ~= 0 then
-                    if not IsJobAllowed() then
-                        SetPedCanBeDraggedOut(seatPed, false)
-                        if Config.Debug then print("[no_dragout] Prevented drag out for seat", i) end
-                    end
+                    SetPedCanBeDraggedOut(seatPed, false)
+                    if Config.Debug then print("[no_dragout] Bloqueo de extracción en asiento", seat) end
                 end
             end
         end
+        ::continue::
     end
 end)
